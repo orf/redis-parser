e3f7476b9ed21f02273be075fdbc0e247d39d27e
Tom Forbes <tom@tomforb.es>
2020-11-15 23:54:27 +0000

    Use preceeded not delimited

--- src/utils.rs
+++ src/utils.rs
@@ -2,27 +2,27 @@ use nom::bytes::streaming::{is_not, take};
 use nom::character::streaming::{char, crlf, digit1};
 use nom::combinator::map_res;
 use nom::number::streaming::double;
-use nom::sequence::{delimited, terminated};
+use nom::sequence::{terminated, preceded};
 use nom::IResult;
 use std::str::{from_utf8, FromStr};
 
 #[inline(always)]
 pub fn parse_str_with_prefix(input: &[u8], prefix: char) -> IResult<&[u8], &str> {
-    map_res(delimited(char(prefix), is_not("\r\n"), crlf), |out| {
+    map_res(preceded(char(prefix), terminated(is_not("\r\n"), crlf)), |out| {
         from_utf8(out)
     })(input)
 }
 
 #[inline(always)]
 pub fn parse_integer_with_prefix(input: &[u8], prefix: char) -> IResult<&[u8], usize> {
-    map_res(delimited(char(prefix), digit1, crlf), |out| {
+    map_res(preceded(char(prefix), terminated(digit1, crlf)), |out| {
         usize::from_str(from_utf8(out).unwrap())
     })(input)
 }
 
 #[inline(always)]
 pub fn parse_double_with_prefix(input: &[u8], prefix: char) -> IResult<&[u8], f64> {
-    delimited(char(prefix), double, crlf)(input)
+    preceded(char(prefix), terminated(double, crlf))(input)
 }
 
 #[inline(always)]
