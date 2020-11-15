a9440ddf15f5fdd830e037973ef57dcb84a4e673
Tom Forbes <tom@tomforb.es>
2020-11-15 22:29:01 +0000

    Fix example

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@
 ///
 /// # Examples
 /// ```
-/// use redis_protocol_parser::resp2::{parse, Resp2Type};
+/// use redis_protocol_parser::resp2::{parse as parse2, Resp2Type};
+/// use redis_protocol_parser::resp3::{parse as parse3, Resp3Type};
 ///
-/// assert_eq!(parse("+test\r\n".as_bytes()), Ok((&b""[..], Resp2Type::String("test"))));
+/// assert_eq!(parse2("+test\r\n".as_bytes()), Ok((&b""[..], Resp2Type::String("test"))));
+/// assert_eq!(parse3("#f\r\n".as_bytes()), Ok((&b""[..], Resp3Type::Boolean(false))));
 /// ```
 pub mod resp2;
 pub mod resp3;
