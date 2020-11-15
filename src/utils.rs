2852c3178aa8ecf8a346c35f4d81a5002e5cb8ac
Tom Forbes <tom@tomforb.es>
2020-11-15 23:45:55 +0000

    Add inline always to utils

--- src/utils.rs
+++ src/utils.rs
@@ -6,26 +6,31 @@ use nom::sequence::{delimited, terminated};
 use nom::IResult;
 use std::str::{from_utf8, FromStr};
 
+#[inline(always)]
 pub fn parse_str_with_prefix(input: &[u8], prefix: char) -> IResult<&[u8], &str> {
     map_res(delimited(char(prefix), is_not("\r\n"), crlf), |out| {
         from_utf8(out)
     })(input)
 }
 
+#[inline(always)]
 pub fn parse_integer_with_prefix(input: &[u8], prefix: char) -> IResult<&[u8], usize> {
     map_res(delimited(char(prefix), digit1, crlf), |out| {
         usize::from_str(from_utf8(out).unwrap())
     })(input)
 }
 
+#[inline(always)]
 pub fn parse_double_with_prefix(input: &[u8], prefix: char) -> IResult<&[u8], f64> {
     delimited(char(prefix), double, crlf)(input)
 }
 
+#[inline(always)]
 pub fn parse_bytes_with_length(input: &[u8], length: usize) -> IResult<&[u8], &[u8]> {
     terminated(take(length), crlf)(input)
 }
 
+#[inline(always)]
 pub fn parse_str_with_length(input: &[u8], length: usize) -> IResult<&[u8], &str> {
     // terminated(map_res!(input, take!($size), $crate::lib::std::str::from_utf8), crlf)(input)
     terminated(map_res(take(length), from_utf8), crlf)(input)
